f1970549fd9008ed27ec90a205397918e4b45e6b
Martin Mitkov <56538513+m-Mitkov@users.noreply.github.com>
2022-07-31 14:57:03 +0300

    .

---
+++ backend/controllers/auth.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.post('/register', async (req, res) => {
+    
+});
+
+router.post('/login', async (req, res) => {
+   
+});
+
+router.get('/logout', (req, res) => {
+
+});
+
+module.exports = router;
